1e646431fa35cb756917e02c8adad46e793323ae
pavan shidole <pavanshidole@gmail.com>
2025-01-12 14:36:31 +0530

    work on templateDriven-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TempDrivenFormComponent } from './shared/components/temp-driven-form/temp-driven-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TempDrivenFormComponent
   ],
   imports: [
     BrowserModule,
